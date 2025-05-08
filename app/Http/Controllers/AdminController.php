490130268bdd21df3f7a9446ad4af216df030d9a
mayanksuntec <mayank.parmar@technoscore.net>
2025-05-08 15:32:42 -0400

    #

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller
 {
     public function login(Request $request)
     {
-        ## test
+        ## test #
         $request->validate([
             'email' => 'required',
             'password' => 'required'
