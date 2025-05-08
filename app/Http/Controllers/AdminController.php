4832fa3541049ca0a2afd0241c6c08ab9f8c353e
mayanksuntec <mayank.parmar@technoscore.net>
2025-05-08 15:27:15 -0400

    Added test comment

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,6 +16,7 @@ class AdminController extends Controller
 {
     public function login(Request $request)
     {
+        ## test
         $request->validate([
             'email' => 'required',
             'password' => 'required'
